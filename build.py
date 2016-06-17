4c36716678a9f63f03b46c4069fcfa7f2f8c8819
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-06-17 23:36:33 +0100

    Añadidas secciones principales del README al pdf.

--- build.py
+++ build.py
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Install dependencies (Ubuntu): sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-lang-spanish
+# Install dependencies (Ubuntu): 
+# sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-lang-spanish
+# sudo pip3 install pypdf2
 
 import re
 import os
 import tempfile
 from subprocess import call
 
-def build_monolitic_file():
+def generate_chapters_pdf(chapters_pdf_path):
     chapters_directory = 'manuscrito'
 
     (_, monolitic_filepath) = tempfile.mkstemp()
@@ -16,6 +18,8 @@ def build_monolitic_file():
         for filepath in sorted(os.listdir(chapters_directory)):
             with open(os.path.join(chapters_directory, filepath), 'rU') as chapter_file:
                 copy_chapter_content(chapter_file, monolitic_file)
+
+    call(["pandoc"] + ["-V", "lang=es", "--toc", "--chapters", "--output", chapters_pdf_path, monolitic_filepath])
 
     return monolitic_filepath
 
@@ -31,7 +35,34 @@ def copy_chapter_content(chapter_file, dst_file):
         dst_file.write(line)
         i += 1
 
-if __name__ == "__main__":
-    monolitic_markdown_filepath = build_monolitic_file()
+def generate_readme_sections_pdf(dst_file):
+    section_headers = ['## Sinopsis', '## Licencia', '## Agradecimientos']
 
-    call(["pandoc"] + ["-V", "lang=es", "--toc", "--chapters", "--output", 'cuarto-poder.pdf', monolitic_markdown_filepath])
+    (_, temp_filepath) = tempfile.mkstemp()
+
+    with open(temp_filepath, 'w') as temp_file:
+        with open('README.md', 'rU') as readme_file:
+            copy_line = False
+            for line in readme_file:
+                if line.startswith('#'):
+                    if copy_line:
+                        temp_file.write('\\newpage')
+                    copy_line = line[:-1] in section_headers
+
+                if copy_line:
+                    if line.startswith('#'):
+                        temp_file.write(line[1:]) # Make header top level.
+                    else:
+                        temp_file.write(line)
+
+    call(["pandoc"] + ["--output", dst_file, temp_filepath])
+
+if __name__ == "__main__":
+    generate_readme_sections_pdf('primeras-secciones.pdf')
+    generate_chapters_pdf('capítulos.pdf')
+
+    from PyPDF2 import PdfFileMerger, PdfFileReader
+    pdf_merger = PdfFileMerger()
+    pdf_merger.append(PdfFileReader(open('primeras-secciones.pdf', 'rb')))
+    pdf_merger.append(PdfFileReader(open('capítulos.pdf', 'rb')))
+    pdf_merger.write("cuarto-poder.pdf")
