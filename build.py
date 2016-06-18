d5b1609f8811d6595590e1709ed2d8bc930aafa6
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-06-18 02:16:17 +0100

    Ficheros PDF temporales ahora se crean en tmp + "Nota final" generado aparte.

--- build.py
+++ build.py
@@ -9,20 +9,24 @@ import os
 import tempfile
 from subprocess import call
 
-def generate_chapters_pdf(chapters_pdf_path, page_offset):
+def generate_chapters_pdf(page_offset):
     chapters_directory = 'manuscrito'
 
     (_, monolitic_filepath) = tempfile.mkstemp()
+    (_, chapters_pdf_filepath) = tempfile.mkstemp()
+    chapters_pdf_filepath += '.pdf'
+    
 
     with open(monolitic_filepath, 'w') as monolitic_file:
         monolitic_file.write('\n\n\\setcounter{page}{%s}\n\n' % page_offset)
         for filepath in sorted(os.listdir(chapters_directory)):
             with open(os.path.join(chapters_directory, filepath), 'rU') as chapter_file:
-                copy_chapter_content(chapter_file, monolitic_file)
+                if filepath.startswith('c'):
+                    copy_chapter_content(chapter_file, monolitic_file)
 
-    call(["pandoc"] + ["-V", "lang=es", "--toc", "--chapters", "--output", chapters_pdf_path, monolitic_filepath])
+    call(["pandoc"] + ["-V", "lang=es", "--toc", "--chapters", "--output", chapters_pdf_filepath, monolitic_filepath])
 
-    return monolitic_filepath
+    return chapters_pdf_filepath
 
 
 def copy_chapter_content(chapter_file, dst_file):
@@ -36,10 +40,12 @@ def copy_chapter_content(chapter_file, dst_file):
         dst_file.write(line)
         i += 1
 
-def generate_readme_sections_pdf(dst_file):
+def generate_readme_sections_pdf():
     section_headers = ['## Sinopsis', '## Licencia', '## Agradecimientos']
 
     (_, temp_filepath) = tempfile.mkstemp()
+    (_, readme_sections_pdf_filepath) = tempfile.mkstemp()
+    readme_sections_pdf_filepath += '.pdf'
 
     with open(temp_filepath, 'w') as temp_file:
         temp_file.write('\\pagenumbering{gobble}\n\n') # No page numbers.
@@ -60,15 +66,36 @@ def generate_readme_sections_pdf(dst_file):
 
         temp_file.write('\\mbox{}\n\n\\thispagestyle{empty}\n\n\\newpage\n\n')
 
-    call(["pandoc"] + ["--output", dst_file, temp_filepath])
+    call(["pandoc"] + ["--output", readme_sections_pdf_filepath, temp_filepath])
+
+    return readme_sections_pdf_filepath
+
+def generate_end_note_pdf(page_offset):
+    (_, temp_filepath) = tempfile.mkstemp()
+    (_, end_note_pdf_filepath) = tempfile.mkstemp()
+    end_note_pdf_filepath += '.pdf'
+
+    with open(temp_filepath, 'w') as temp_file:
+        with open('manuscrito/nota-final.md', 'rU') as src_file:
+            temp_file.write('\n\n\\setcounter{page}{%s}\n\n' % page_offset)
+            for line in src_file:
+                if line == '## Navegación\n':
+                    break
+                temp_file.write(line)
+
+    call(["pandoc"] + ["--output", end_note_pdf_filepath, temp_filepath])
+
+    return end_note_pdf_filepath
 
 if __name__ == "__main__":
-    generate_readme_sections_pdf('primeras-secciones.pdf')
-    generate_chapters_pdf('capítulos.pdf', page_offset=8)
+    readme_sections_pdf_filepath = generate_readme_sections_pdf()
+    chapters_pdf_filepath = generate_chapters_pdf(page_offset=8)
+    end_note_pdf_filepath = generate_end_note_pdf(page_offset=143)
 
     from PyPDF2 import PdfFileMerger, PdfFileReader
     pdf_merger = PdfFileMerger()
     pdf_merger.append(PdfFileReader(open('portada.pdf', 'rb')))
-    pdf_merger.append(PdfFileReader(open('primeras-secciones.pdf', 'rb')))
-    pdf_merger.append(PdfFileReader(open('capítulos.pdf', 'rb')))
+    pdf_merger.append(PdfFileReader(open(readme_sections_pdf_filepath, 'rb')))
+    pdf_merger.append(PdfFileReader(open(chapters_pdf_filepath, 'rb')))
+    pdf_merger.append(PdfFileReader(open(end_note_pdf_filepath, 'rb')))
     pdf_merger.write("cuarto-poder.pdf")
